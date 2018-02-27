6fd24c5d3dcbff36f37a14bf8f8d468b09704ecb
Lisa <lisa.michelle.nash@gmail.com>
2018-02-26 18:19:06 -0600

    ?? don't know why formatting is weird, but at least we can get urls.

--- inside_ideo_people.py
+++ inside_ideo_people.py
@@ -9,30 +9,37 @@ import requests
 import requests_cache
 import socket
 import sys
+import sys
 from bs4 import BeautifulSoup
 from dropbox import settings as dropbox_settings
+from lxml import etree
 from tqdm import *
-import sys
 
 if __name__ == '__main__':
     sys.setrecursionlimit(3000)
 
-    target_url = 'https://inside.ideo.com/users/search?user_location_ids%5B%5D=3'
-    response = requests.get(target_url,
-                            headers=dropbox_settings.HEADERS,
-                            timeout=5,
-                            )
+    # target_url = 'https://inside.ideo.com/users/search?user_location_ids%5B%5D=3'
+    target_url2 = 'https://inside.ideo.com/users/search?_=1519678038902&page=2&replace=false&sort=relevance&sort_dir=desc&sort_direction_name=desc&user_location_ids%5B%5D=3'
+    target_url1 = 'https://inside.ideo.com/users/search?_=1519678038902&page=1&replace=false&sort=relevance&sort_dir=desc&sort_direction_name=desc&user_location_ids%5B%5D=3'
 
-    #print(response.content)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    target_urls = [target_url1, target_url2]
 
-    people_info = soup.find_all('div')
+    people_urls = []
 
-    for entry in people_info:
-        print(entry.strip)
-    # target_url = 'https://slate.com/'
-    # response = requests.get(target_url)
-    #
-    # #print(response.content)
-    # soup = BeautifulSoup(response.content, "html.parser")
-    # print(soup.find("div"))
+    for target_url in target_urls:
+        response = requests.get(target_url,
+                                headers=dropbox_settings.HEADERS,
+                                timeout=5,
+                                )
+
+        soup = BeautifulSoup(response.text, "lxml")
+
+        people_info = soup.find_all('a', {'class': ['\\"js-headshot-wrapper\\"']})
+
+        for div in people_info:
+            people_urls.append(div['href'].strip())
+
+    count = 0
+    for url in people_urls:
+        print(count, url)
+        count += 1
