594d4f24879394d8a12303667f052c6710d6b453
Lisa <lisa.michelle.nash@gmail.com>
2018-03-13 21:53:51 -0500

    No lunches with BL!!

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ DATA_DIRECTORY = './data/'
 
 inside_ideo_json = DATA_DIRECTORY + 'people_info.json'
 inside_ideo_csv = DATA_DIRECTORY + 'people_info.csv'
+bl_list_csv = DATA_DIRECTORY + 'BLs.csv'
 chideo_directory = DATA_DIRECTORY + 'ChIDEO_directory.csv'
 
 # where to save the groups
@@ -13,7 +14,6 @@ if not os.path.exists(save_directory):
 
 number_in_group = 3
 min_disciplines = 2
-weight_factor = 50
 min_meetings = 1
 max_meetings = 2
 new_hire_days = 180
--- triad_optimization.py
+++ triad_optimization.py
@@ -2,12 +2,15 @@ import json
 import numpy as np
 import pandas as pd
 import settings
+import sys
 from datetime import datetime, timedelta
 
 
 class BatchGenerator:
     def __init__(self, batch_settings):
         self.people_info_df = pd.read_csv(batch_settings.inside_ideo_csv, parse_dates=['hired_at'])
+        self.people_info_df = self.recode_disciplines()
+        self.BL_list = pd.read_csv(batch_settings.bl_list_csv)
         self.directory = pd.read_csv(batch_settings.chideo_directory, parse_dates=['Anniversary'],
                                      encoding="ISO-8859-1")
         with open(batch_settings.inside_ideo_json) as json_data:
@@ -20,7 +23,6 @@ class BatchGenerator:
         self.number_in_group = batch_settings.number_in_group
         self.min_disciplines = batch_settings.min_disciplines
         self.new_hire_days = batch_settings.new_hire_days
-        self.weight_factor = batch_settings.weight_factor
 
         self.number_of_meetings_dict = batch_settings.number_of_meetings_dict
 
@@ -29,20 +31,30 @@ class BatchGenerator:
         # score_weights = batch_settings.score_weights  # TODO: Make this a real thing later
 
     def sample_df(self, df_to_sample, number_to_sample):
-        weight_factor = self.weight_factor
 
         try:
             weight = df_to_sample['max_meetings'] - df_to_sample['number_of_meetings']
             output = df_to_sample.sample(number_to_sample,
-                                         weights=weight)
+                                         weights=weight ** 6)
         except ValueError:
             output = df_to_sample
         return output
 
-    #TODO: write this
-    #def recode_disciplines(self):
+    def recode_disciplines(self):
         # recodes support disicplines
+        support_discipline_list = [{'discipline': {'Talent': 'Support'}},
+                                   {'discipline': {'Marketing': 'Support'}},
+                                   {'discipline': {'Coordination': 'Support'}},
+                                   {'discipline': {'Experience': 'Support'}},
+                                   {'discipline': {'Accounting': 'Support'}}]
 
+        recoded_df = self.people_info_df.copy()
+
+        for i in range(len(support_discipline_list)):
+            replacement_dict = support_discipline_list[i]
+            recoded_df = recoded_df.replace(to_replace=replacement_dict)
+
+        return recoded_df
 
     def calculate_triad_score(self, triad):
         # number of disciplines
@@ -83,18 +95,18 @@ class BatchGenerator:
         journey_number = {'Individual': 1,
                           'Team': 2,
                           'Director': 3,
-                          'Enterprise': 4}
+                          'Enterprise': 3}
 
         triad_journies = np.array([journey_number[i] for i in triad_data.Journey])
 
         if len(triad_journies[triad_journies >= 3]) > 1:
-            sub = 1
+            sub = 2
         else:
             sub = 0
 
         num_journies = np.max(triad_journies) - np.min(triad_journies) - sub
 
-        hire_delta = datetime.now() - triad_data['Anniversary']
+        hire_delta = datetime.now() - triad_data['hired_at']
         num_new_hires = (hire_delta < pd.Timedelta(days=180)).sum() / len(triad_data)
 
         triad_score = discipline_weight * num_disciplines + journey_weight * num_journies \
@@ -139,8 +151,27 @@ class BatchGenerator:
 
     def check_score(self, triad):
         score = self.calculate_triad_score(triad)
+        bl_check = self.check_bl(triad)
 
-        return score > self.min_score, score
+        return (score > self.min_score and bl_check), score
+
+    def check_bl(self, triad):
+        # make pairs from triad
+        two_list = []
+        email_ad = triad.email_address.values
+        for i in range(len(email_ad)):
+            pairs = [frozenset([email_ad[i], email_ad[j]]) for j in range(i + 1, len(triad))]
+            two_list += pairs
+
+        # check against BL
+        two_list = set(two_list)
+
+        BL_list = self.BL_list
+        BL_list = set([frozenset(BL_list.iloc[i].values) for i in range(len(BL_list))])
+
+        BL_intersection = two_list.intersection(BL_list)
+
+        return len(BL_intersection) == 0
 
     def generate_batch(self):
         batch_df = self.combined.copy()
@@ -166,12 +197,21 @@ class BatchGenerator:
             good_group = False
             iterations = 0  # let's not get stuck in this loop forever.
 
+            best_group = []
+            high_score = -100
             while (not good_group and iterations < 10):
                 triad, batch_df = self.generate_single(batch_df)
                 good_group, group_score = self.check_score(triad)
+                self.check_bl(triad)
+
+                if group_score > high_score:
+                    best_group = triad
+                    high_score = group_score
 
                 iterations += 1
 
+            triad = best_group
+            group_score = high_score
             batch_df.loc[
                 triad.index, 'number_of_meetings'] += 1
 
@@ -186,74 +226,17 @@ if __name__ == '__main__':
 
     triads, scores = batch_generator.generate_batch()
 
+    print(triads[0])
+
+    file_data = []
     for i in range(len(triads)):
-        print(triads[i], scores[i])
-        print(' ')
+        # print(triads[i], scores[i])
+        # print(triads[i]['email_address'])
+        emails = list(triads[i]['email_address'].values)
+        file_data.append(emails)
 
+    print(file_data)
 
-#
-# def generate_random_list(batch_settings):
-#     number_in_group = batch_settings.number_in_group
-#     max_meetings = batch_settings.max_meetings
-#     weight_factor = batch_settings.weight_factor
-#
-#     people_info_df = pd.read_csv(settings.inside_ideo_csv, parse_dates=['hired_at'])
-#
-#     people_info_df['number_of_meetings'] = 0  # keep track of how many groups someone is in
-#
-#     suggested_triads = []
-#
-#     while (sum(people_info_df.number_of_meetings < 1) > 0):
-#         # exclude everyone who's been paired too many times
-#         people_info_df = people_info_df[people_info_df.number_of_meetings < max_meetings]  # these people are eligible
-#
-#         triad = sample_df(people_info_df,
-#                           number_in_group, weight_factor)
-#
-#         people_info_df.loc[
-#             triad.index, 'number_of_meetings'] += 1  # increment the meeting count for people
-#         suggested_triads.append(triad.email_address.tolist())  # add this triad to the list!
-#
-#     suggested_triad_df = pd.DataFrame(suggested_triads, columns=['person_1', 'person_2', 'person_3'])
-#
-#     return suggested_triad_df
-#
-#
-# if __name__ == '__main__':
-#     directory = pd.read_csv(settings.chideo_directory, parse_dates=['Anniversary'])
-#     inside_ideo = pd.read_csv(settings.inside_ideo_csv)
-#     with open(settings.inside_ideo_json) as json_data:
-#         project_lists = json.load(json_data)
-#
-#     combined = inside_ideo.merge(directory, left_on='email_address', right_on='Email')
-#
-#     combined = combined[['email_address', 'Journey', 'discipline', 'Anniversary']]
-#
-#     highest = 0
-#
-#     highest_grouping = []
-#     lowest_grouping = []
-#
-#     for i in range(20):
-#         triads = generate_random_list(settings)
-#
-#         score_list = calculate_triad_score(triads, combined, project_lists)
-#
-#         score = np.median(np.array(score_list))
-#         triads['score'] = score_list
-#
-#         if i == 0:
-#             lowest = score
-#
-#         if score > highest:
-#             highest = score
-#             highest_grouping = triads
-#
-#         if score < lowest:
-#             lowest = score
-#             lowest_grouping = triads
-#
-#     print(lowest, highest)
-#     highest_grouping.to_csv(settings.save_directory + 'best_grouping_generate_random_list.csv', index=False)
-#     lowest_grouping.to_csv(settings.save_directory + 'worst_grouping_generate_random_list.csv', index=False)
-#     print('saved ', settings.save_directory + 'best_grouping_generate_triad_group.csv')
+    suggested_triad_df = pd.DataFrame(file_data, columns=['person_1', 'person_2', 'person_3'])
+    suggested_triad_df['score'] = scores
+    suggested_triad_df.to_csv(settings.save_directory + 'suggested_triads.csv', index=False)
