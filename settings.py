eff706a4df83b5caaa99c63cb1892dbf8d01bf3a
jzanzig <jmlzanzig@gmail.com>
2018-04-30 13:38:08 -0500

    Put Loui and Irmak on the do not invite list

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ inside_ideo_json = DATA_DIRECTORY + 'people_info.json' # this json has the proje
 inside_ideo_csv = DATA_DIRECTORY + 'people_info.csv'
 bl_list_csv = DATA_DIRECTORY + 'BLs.csv'
 chideo_directory = DATA_DIRECTORY + 'ChIDEO_directory.csv'
-suggested_triads = DATA_DIRECTORY + 'suggested_triads_2.csv'
-#suggested_triads = 'suggested_triads_test.csv'
+#suggested_triads = DATA_DIRECTORY + 'suggested_triads_test.csv'
+suggested_triads = 'suggested_triads_test.csv'
 
 # where to save the groups
 save_directory = DATA_DIRECTORY + 'previous_groupings/'
@@ -44,6 +44,8 @@ number_of_meetings_dict = {'mandywong@ideo.com': 0,  # Old intern?
 
                            'fgerlach@ideo.com': 0, # Out of studio
                            'gwinther@ideo.com': 0,
+                           'isirer@ideo.com': 0,
+                           'loui@ideo.com': 0
                            }
 
 # calendar settings
@@ -52,16 +54,16 @@ earliest_time = 12 # when is the latest the meeting should begin?
 latest_time = 14 # when is the latest the meeting should END?
 time_window = 30 # how many days out should we search for appropriate times?
 event_name = 'Meet n\' Three!'
-event_description = """Hello! Meaty the Meetbot here, inviting you all to go out to lunch. I chose a time that looked 
-open on everyone's calendars, but I am only a prototype and I realize that Google calendar is not necessarily an 
-accurate reflection of everyone's life. If this time doesn't work for you, please coordinate with each other to find a new day! 
+event_description = """Hello! Meaty the Meetbot here, inviting you all to go out to lunch. This time looked open on 
+everyone's calendars, but keep in mind I'm only a prototype! I realize that Google calendar is not necessarily an 
+accurate reflection of everyone's life. If this doesn't work for you, please coordinate with each other to find another time! 
 
-You can try an establishment from our <a href="https://docs.google.com/document/d/1812VOM-ANeDWk0eCCgni5HOvzTEWhEgIurWL0tJqBag/edit">curated list</a>, or you can choose your own adventure!
+You can try a place from our <a href="https://docs.google.com/document/d/1812VOM-ANeDWk0eCCgni5HOvzTEWhEgIurWL0tJqBag/edit">curated list</a>, or choose your own adventure!
 
-If you don't have time or would rather not participate, please just opt out by declining the invitation. 
+This is voluntary! If you are too busy or would rather not participate, feel free to decline the invitation and opt out. 
 
-The fine print: Please stick to a budget of about $20/person and expense the cost to Chicago Talent in Concur.
+Please stick to a budget of about $20/person and expense the cost to Chicago Talent in Concur.
 
 Happy FaceSlacking!"""
 
-# TODO: try adding
+# TODO: try adding attachment
