1f4597893b7033d82c3e1a27372200054eec1908
jzanzig <jmlzanzig@gmail.com>
2018-06-19 09:00:50 -0500

    Update settings for D4AI calls, remove group generation settings

--- settings.py
+++ settings.py
@@ -9,60 +9,13 @@ chideo_directory = DATA_DIRECTORY + 'ChIDEO_directory.csv'
 suggested_triads = DATA_DIRECTORY + 'email_list.csv'
 #suggested_triads = 'suggested_triads_test.csv'
 
-# where to save the groups
-save_directory = DATA_DIRECTORY + 'previous_groupings/'
-if not os.path.exists(save_directory):
-    os.mkdir(save_directory)
-
-# settings for batch generation
-number_in_group = 3
-min_disciplines = 2
-min_meetings = 1
-max_meetings = 2
-new_hire_days = 180
-
-# scoring settings
-min_score = 0.9
-score_weights = {'discipline': 3,
-                 'journey': 1,
-                 'new_hire': 0,
-                 'core_project': -2}
-
-# settings for ideal group
-ideal_group = {'discipline': 3,
-               'journey': 3,
-               'new_hire': 1,
-               'core_project': 0}
-
-# special settings
-# Here you can force one person's weights to zero
-number_of_meetings_dict = {'mandywong@ideo.com': 0,  # Old intern?
-                           'matthewgs@ideo.com': 0, # Matthew is on Inside IDEO twice
-                           'dlee@ideo.com': 0,
-                           'fgerlach@ideo.com': 0, # Out of studio
-                           'gwinther@ideo.com': 0,
-                           'isirer@ideo.com': 0,
-                           'loui@ideo.com': 0,
-                           'gmitchell@ideo.com': 0,
-                           'bsyverson@ideo.com': 0
-                           }
-
 # calendar settings
-event_duration = 60 # how long should the meeting last? (in minutes)
+event_duration = 30 # how long should the meeting last? (in minutes)
 earliest_time = 12 # when is the latest the meeting should begin?
 latest_time = 13 # when is the latest the meeting should END?
-time_window = 30 # how many days out should we search for appropriate times?
-event_name = 'Meet n\' Three!'
-event_description = """Hello! Meaty the Meetbot here, inviting you all to go out to lunch. This time looked open on 
-everyone's calendars, but keep in mind I'm only a prototype! I realize that Google calendar is not necessarily an 
-accurate reflection of everyone's life. If this doesn't work for you, please coordinate with each other to find another time! 
-
-You can try a place from our <a href="https://docs.google.com/document/d/1812VOM-ANeDWk0eCCgni5HOvzTEWhEgIurWL0tJqBag/edit">curated list</a>, or choose your own adventure!
-
-This is voluntary! If you are too busy or would rather not participate, feel free to decline the invitation and opt out. 
-
-Please stick to a budget of about $20/person and expense the cost to Chicago Talent in Concur.
-
-Happy FaceSlacking!"""
+time_window = 60 # how many days out should we search for appropriate times?
+event_name = 'D4AI Global Update Call!'
+event_description = """D4AI Global Call! Text goes here.
+"""
 
 
